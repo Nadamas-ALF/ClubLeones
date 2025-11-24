513609ad7f69c262177d16f1712d8f5dde70d5af
ALF <158531864+Nadamas-ALF@users.noreply.github.com>
2025-11-24 02:15:20 -0600

    Migrate DB to Oracle, add procedures and update UI
    Logica de mantenimiento_provincias.
    se Puede Probar minimamente pero hace falta la creacion de las otras partes de mantenimiento para probar correctamente.

---
+++ SQL/PoblacionDeTablasManual.sql
@@ -0,0 +1,16 @@
+
+--Poblar tabla provincia 
+INSERT INTO provincias (nombre_provincia) VALUES ('San Jose');
+INSERT INTO provincias (nombre_provincia) VALUES ('Alajuela');
+INSERT INTO provincias (nombre_provincia) VALUES ('Cartago');
+INSERT INTO provincias (nombre_provincia) VALUES ('Heredia');
+INSERT INTO provincias (nombre_provincia) VALUES ('Guanacaste');
+INSERT INTO provincias (nombre_provincia) VALUES ('Puntarenas');
+INSERT INTO provincias (nombre_provincia) VALUES ('Limón');
+
+
+
+
+
+
+
